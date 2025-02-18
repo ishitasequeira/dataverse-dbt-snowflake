adf0a3e28416bc0118a69310cec6149d05de18b1
Ishita Sequeira <46771830+ishitasequeira@users.noreply.github.com>
2025-02-17 21:33:18 -0500

    Delete orders directory

--- orders/models/orders/orders_per_city_per_customer.sql
+++
@@ -1,7 +0,0 @@
-with orders_per_city_per_customer as (
-
-     select city, customer_name, count(city) FROM ORDERS.GOOGLE_SHEETS.ORDERS GROUP BY CITY, customer_name
-
-)
-
-select * from orders_per_city_per_customer
--- orders/models/orders/orders_per_city_per_day.sql
+++
@@ -1,8 +0,0 @@
-with orders_per_city_per_day as (
-
-     select city, ORDER_DATE, count(city) FROM ORDERS.GOOGLE_SHEETS.ORDERS GROUP BY CITY, ORDER_DATE
-
-)
-
-
-select * from orders_per_city_per_day
